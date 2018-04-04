4c88b0ce65012951fb42dcdd13b9cf8cb6c4445d
kn45 <381404825@qq.com>
2018-04-04 10:58:45 +0800

    add pred

--- 1_GetTsv.sh
+++ 1_GetTsv.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-data_raw=data_all/round1_ijcai_18_train_20180301.txt
+data_all_raw=data_all/round1_ijcai_18_train_20180301.txt
 data_all=data_all/data_all.tsv
-cat $data_raw | awk '{if (NR>1) print $0}' | sed -e 's/ /	/g' > $data_all
+
+data_pred_raw=data_all/round1_ijcai_18_test_a_20180301.txt
+data_pred=data_pred/data_pred.tsv
+
+
+cat $data_all_raw | awk '{if (NR>1) print $0}' | sed -e 's/ /	/g' > $data_all
+
+cat $data_pred_raw | awk '{if (NR>1) print $0}' | sed -e 's/ /	/g' > $data_pred
